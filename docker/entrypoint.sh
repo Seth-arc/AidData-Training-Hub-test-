42dcc863d5bb6c3d769709a04bd6d4082cad7bf7
Seth-arc <64438772+Seth-arc@users.noreply.github.com>
2026-01-22 00:47:41 -0500

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
+
 : "${PORT:=80}"
+echo "Nginx will listen on port: $PORT"
 
 # Substitute environment variables in Nginx config
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
